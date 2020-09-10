c25bac94375f366daef08c14091100bd0a610d48
mertgeren <mert.geren@iqb.solutions>
2020-09-10 16:16:38 +0300

    singleton design pattern added

--- LogCleaner/Startup.cs
+++ LogCleaner/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using LogCleaner.ScheduleTask;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace LogCleaner
 {
@@ -25,7 +19,7 @@ namespace LogCleaner
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddSingleton<IHostedService, CleanerTask>();        // singleton design pattern kullanımı için tanımlanmalı
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
